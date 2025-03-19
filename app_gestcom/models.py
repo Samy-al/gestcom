e8cd7c155a28d56bcdcc37c6af4f31e50355e37c
mukhut <mukhutjp@gmail.com>
2025-03-19 11:07:01 +0400

    model categorie

--- app_gestcom/models.py
+++ app_gestcom/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Categorie(models.Model):
+    code = models.CharField(max_length=10,primary_key=True)
+    designation = models.CharField(max_length=100,unique=True)
